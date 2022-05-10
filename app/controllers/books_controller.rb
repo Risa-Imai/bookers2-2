59f69751d3e49dc3a109f6d713f9a15ae6002521
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-05-10 12:08:03 +0000

    途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,33 @@
 class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
